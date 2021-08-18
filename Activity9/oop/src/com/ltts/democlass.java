e3f4d4c5781913d5a4bd81b17c6d08e8181bd0b8
dikshithcs123 <86772460+dikshithcs123@users.noreply.github.com>
2021-08-18 22:06:53 +0530

    Add files via upload

---
+++ Activity9/oop/src/com/ltts/democlass.java
@@ -0,0 +1,13 @@
+package com.ltts;
+
+public class democlass {
+	public static void main(String[] args) {
+		System.out.println("hi");
+		
+		Student s = new Student();
+		s.setAge(22);
+		s.setName("Dikshith C S");
+		System.out.println(s);
+	}
+
+}
