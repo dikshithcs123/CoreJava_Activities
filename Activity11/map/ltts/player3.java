b64beb81f903d40347c9c43801632ddbade53345
dikshithcs123 <86772460+dikshithcs123@users.noreply.github.com>
2021-08-18 22:12:01 +0530

    Add files via upload

---
+++ Activity11/map/ltts/player3.java
@@ -0,0 +1,14 @@
+package act11.map.ltts; 
+
+public class player3 {
+	int id;    
+	String name, team, skill;    
+  
+	public player3(int id, String name, String team, String skill) {    
+	    this.id = id;    
+	    this.name = name;    
+	    this.team = team;    
+	    this.skill = skill;    
+
+}
+}
